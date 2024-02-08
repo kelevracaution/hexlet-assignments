9ce47ea7eae1625356b9eb2876c90a5e21b862af
@hexlet/cli <support@hexlet.io>
2024-02-08 15:07:56 +0500

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,36 @@
+require 'uri'
+require 'forwardable'
+
+class Url
+
+  include Comparable
+  attr :url
+  def <=>(other)
+    self.query_params <=> other.query_params
+  end
+
+  attr_reader :url
+
+  def initialize(str)
+    @url = URI(str)
+  end
+
+  extend Forwardable
+  def_delegators :@url, :host, :scheme, :port
+
+  def query_params
+    hash = {}
+    @url.to_s
+        .partition('?').last
+        .split('&').each do |el|
+          key, value = el.split('=')
+          hash[key] = value
+    end
+    hash
+  end
+
+  def query_param(key,value = nil)
+    self.query_params.key?(key.to_s) ? self.query_params[key.to_s] : value
+  end
+
+end
